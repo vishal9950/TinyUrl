cc863062ff9f1cfa78d240933514a105137a54dd
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-06 09:00:56 +0530

    Test getHash to retur the hash of a string

--- tests/helpers/getash.test.js
+++ tests/helpers/getash.test.js
@@ -1,9 +1,15 @@
 const getHash = require('../../src/helpers/getHash');
+const crypto = require('crypto');
 
 describe('Test function getHash: ', () => {
   test('Should return a string: ', () => {
     const hash = getHash('dwdfc');
     expect(typeof hash).toBe('string');
+  });
+
+  test('Should return the hash: ', () => {
+    const hash = crypto.createHash('md5').update('abc').digest('hex');
+    expect(getHash('abc')).toBe(hash);
   });
 });
 
