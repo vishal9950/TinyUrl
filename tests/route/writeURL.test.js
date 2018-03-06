debd8a6287cc960cb9104800f27ad19596593979
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-06 09:09:10 +0530

    Test POST /write to return the hash of the existing url

--- tests/route/writeURL.test.js
+++ tests/route/writeURL.test.js
@@ -1,4 +1,5 @@
 const Server = require('../../src/server');
+const getHash = require('../../src/helpers/getHash');
 
 describe('Test server for route /write: ', () => {
   test('Should return the hash for an existing url: ', (done) => {
@@ -12,6 +13,21 @@ describe('Test server for route /write: ', () => {
 
     Server.inject(options, (response) => {
       expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('Should return the hash for an existing url: ', (done) => {
+    const options = {
+      url: '/write',
+      method: 'POST',
+      payload: {
+        longurl: 'http://somerandomurl6',
+      },
+    };
+    const hash = getHash('http://somerandomurl6').slice(0, 6);
+    Server.inject(options, (response) => {
+      expect(response.result).toBe(hash);
       done();
     });
   });
