82ab56e2de9802ca250db7e09c54943525795ceb
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-06 10:57:25 +0530

    Test recursiveCreate to return shorturl for an existing url

--- tests/helpers/recursiveCreate.test.js
+++ tests/helpers/recursiveCreate.test.js
@@ -1,9 +1,35 @@
 const recursiveCreate = require('../../src/helpers/recursiveCreate');
+const Models = require('../../models');
 // const getHash = require('../../src/helpers/getHash');
 const crypto = require('crypto');
 
 describe('Test function recursiveCreate: ', () => {
+  beforeEach(() => {
+    Models.urls.destroy({
+      where: {
+        longurl: 'http://newurl6',
+      },
+    });
+  });
+  afterAll(() => {
+    Models.urls.destroy({
+      where: {
+        longurl: 'http://newurl6',
+      },
+    });
+  });
   test('Should return the shorturl for new url: ', () => {
+    const longUrl1 = 'http://newurl6';
+    const hash = crypto.createHash('md5').update(longUrl1).digest('base64').replace(/\//g, '_');
+    console.log(hash);
+    const result = recursiveCreate(longUrl1, hash, 0, 6);
+    console.log(result);
+    result.then((value) => {
+      expect(value).toBe(hash.substr(0, 6));
+    });
+  });
+
+  test('Should return the shorturl for an existing url: ', () => {
     const longUrl1 = 'http://newurl6';
     const hash = crypto.createHash('md5').update(longUrl1).digest('base64').replace(/\//g, '_');
     console.log(hash);
