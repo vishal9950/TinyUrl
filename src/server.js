972faaf9e7211d859321c5cd193f3e91fbf488b0
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-07 11:08:55 +0530

    Add Good logs

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 const Hapi = require('hapi');
 const Route = require('./route');
+const Good = require('good');
 
 const server = new Hapi.Server();
 
 server.connection({
-  port: 8000,
+  port: 3000,
   host: 'localhost',
+});
+
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*',
+        }],
+      }, {
+        module: 'good-console',
+      }, 'stdout'],
+    },
+  },
+}, (err) => {
+  if (err) {
+    throw err;
+  }
 });
 
 server.route(Route);
